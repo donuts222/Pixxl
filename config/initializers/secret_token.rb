d9eb97c0bfed457b646accc0e21a17f45211a929
Michael Godek <michael.godek@gmail.com>
2013-08-27 15:38:58 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pixxl::Application.config.secret_token = '2b896d13acbf51c1d3802336118c46f64767ce6e6c8453de1dad3e8cdaa8cd953898a9d7a75a70c5f2eaf6128fd4bbc74f933a41e37286a70b86b6c619661b36'
